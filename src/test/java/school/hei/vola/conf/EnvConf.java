fd0ac58813ac17acdfbacfb6e8fd4e28b14e9076
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-12-09 15:00:08 +0300

    chore(test): replace test data

--- src/test/java/school/hei/vola/conf/EnvConf.java
+++ src/test/java/school/hei/vola/conf/EnvConf.java
@@ -9,7 +9,6 @@ public class EnvConf {
         "spring.datasource.url", () -> "jdbc:h2:mem:testdb;CASE_INSENSITIVE_IDENTIFIERS=TRUE");
     registry.add("spring.datasource.driverClassName", () -> "org.h2.Driver");
     registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.H2Dialect");
-    registry.add(
-        "orange.api.url", () -> "dummy");
+
   }
 }
--- src/test/java/school/hei/vola/conf/TestData.java
+++ src/test/java/school/hei/vola/conf/TestData.java
@@ -3,5 +3,6 @@ package school.hei.vola.conf;
 public class TestData {
   public static final String ORANGE_REF_SUCCEEDED =
       // note(unique_pspPayment)
-      "MP250909.1102.A61439";
+      //Changed on waiting of scraper fix
+      "MP251208.1029.B48284";
 }
