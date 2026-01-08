db94a412bc6fefb62ce0e78cf4261ab8d95b6e80
Tiavina Andriamamivony <hei.tiavina.1@gmail.com>
2026-01-08 14:08:35 +0300

    chore: add debugging logs

--- src/main/java/school/hei/vola/model/psp/orange/OrangeApiClient.java
+++ src/main/java/school/hei/vola/model/psp/orange/OrangeApiClient.java
@@ -28,19 +28,29 @@ public class OrangeApiClient {
             "/transactions?date=%s-%s-%s",
             date.getYear(), date.getMonthValue(), date.getDayOfMonth());
     var uri = baseUrl + path;
+    log.info("Building Orange API request with baseUrl: {}, path: {}", baseUrl, path);
+    log.info("Full URI: {}", uri);
+
     var httpRequest = HttpRequest.newBuilder().uri(URI.create(baseUrl + path)).GET().build();
 
     try (var httpClient = newHttpClient()) {
       log.info("Fetching Orange transactions: {}", uri);
       var response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
       var httpStatus = response.statusCode();
-      log.info("Https status from Orange was: {}", httpStatus);
+      log.info("HTTP status from Orange: {}", httpStatus);
+      log.info("Response body: {}", response.body());
+
       if (httpStatus / 100 != 2) {
+        log.error(
+            "Non-2xx response from Orange API: status={}, body={}", httpStatus, response.body());
         throw new OrangeApiException("Response from Orange was not 2xx: " + response);
       }
 
-      return om.readValue(response.body(), OrangeDailyTransactions.class);
+      var dailyTransactions = om.readValue(response.body(), OrangeDailyTransactions.class);
+      log.info("Successfully parsed {} transactions", dailyTransactions.getTransactions().size());
+      return dailyTransactions;
     } catch (IOException | InterruptedException e) {
+      log.error("Error calling Orange API for date {}: {}", date, e.getMessage(), e);
       throw new RuntimeException(e);
     }
   }
