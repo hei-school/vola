910a58adc454185945709146d9c4942a2477a248
SalomiaZK <salomia.razakarivony@gmail.com>
2026-04-08 14:36:15 +0300

    chore: remove wildCard

--- src/test/java/school/hei/vola/service/PaymentServiceIT.java
+++ src/test/java/school/hei/vola/service/PaymentServiceIT.java
@@ -1,7 +1,11 @@
 package school.hei.vola.service;
 
 import static java.util.UUID.randomUUID;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
