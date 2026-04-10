02fc742c872c63879c311608e4fdeedbcc312738
SalomiaZK <salomia.razakarivony@gmail.com>
2026-04-10 09:15:32 +0300

    test: add OrangeTransactionsImportRequestedService

--- src/test/java/school/hei/vola/service/event/OrangeTransactionImportRequestedServiceIT.java
+++ src/test/java/school/hei/vola/service/event/OrangeTransactionImportRequestedServiceIT.java
@@ -2,26 +2,31 @@ package school.hei.vola.service.event;
 
 import static java.nio.file.Files.readAllBytes;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.inOrder;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import school.hei.vola.conf.FacadeIT;
+import school.hei.vola.endpoint.event.model.OrangeTransactionsImportRequested;
 import school.hei.vola.file.bucket.BucketComponent;
+import school.hei.vola.model.ImportedTransactionDetails;
 import school.hei.vola.repository.OrangePaymentRepository;
 import school.hei.vola.service.MultipartFileConverter;
 import school.hei.vola.service.utils.ExcelParser;
 
-public class OrangeTransactionImportRequestedIT extends FacadeIT {
-  @Autowired OrangePaymentRepository orangePaymentRepository;
+public class OrangeTransactionImportRequestedServiceIT extends FacadeIT {
   @MockBean BucketComponent bucketComponent;
   @Autowired MultipartFileConverter multipartFileConverter;
-  @Autowired ExcelParser excelParser;
+  @MockBean ExcelParser excelParser;
+  @Autowired OrangeTransactionsImportRequestedService orangeTransactionsImportRequestedService;
+  @MockBean OrangePaymentRepository orangePaymentRepository;
 
   @Test
   void save_transactions_imported() throws IOException {
@@ -34,12 +39,16 @@ public class OrangeTransactionImportRequestedIT extends FacadeIT {
             readAllBytes(path));
     var file = multipartFileConverter.apply(multipartFile);
     var bucketKey = "/TRANSACTIONS_IMPORT_XLS/" + file.getName();
+    var result = new ImportedTransactionDetails(List.of(), List.of());
     when(bucketComponent.download(any())).thenReturn(file);
-    var inOrder = inOrder(bucketComponent, excelParser, orangePaymentRepository);
+    when(excelParser.parseToOrangeTransaction(file)).thenReturn(result);
+    when(orangePaymentRepository.saveAll(anyList())).thenReturn(List.of());
 
-    inOrder.verify(bucketComponent).download(bucketKey);
-    inOrder.verify(excelParser).parseToOrangeTransaction(file);
-    var validTransactions = excelParser.parseToOrangeTransaction(file).validTransactions();
-    inOrder.verify(orangePaymentRepository).saveAll(validTransactions);
+    orangeTransactionsImportRequestedService.accept(
+        new OrangeTransactionsImportRequested(bucketKey));
+
+    verify(excelParser).parseToOrangeTransaction(file);
+    verify(bucketComponent).download(bucketKey);
+    verify(orangePaymentRepository).saveAll(List.of());
   }
 }
