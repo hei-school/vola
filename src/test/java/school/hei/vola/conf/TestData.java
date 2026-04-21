44958c132314e23c50344c049da92c77e31018d3
SalomiaZK <salomia.razakarivony@gmail.com>
2026-04-21 13:48:35 +0300

    chore: fix test

--- src/test/java/school/hei/vola/conf/TestData.java
+++ src/test/java/school/hei/vola/conf/TestData.java
@@ -4,5 +4,5 @@ public class TestData {
   public static final String ORANGE_REF_SUCCEEDED =
       // note(unique_pspPayment)
       // Changed on waiting of scraper fix
-      "MP260409.1619.C06783";
+      "MP260420.0927.B98941";
 }
