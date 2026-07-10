7696eab7b885da33f958f3d0bb1c49b0092a0d29
colombefioren <colomberakotonjanahary@gmail.com>
2026-07-10 13:29:43 +0300

    refactor: resolve blank line conflict

--- src/test/java/school/hei/vola/endpoint/rest/controller/PaymentControllerIT.java
+++ src/test/java/school/hei/vola/endpoint/rest/controller/PaymentControllerIT.java
@@ -120,6 +120,7 @@ class PaymentControllerIT extends FacadeIT {
     try {
       orangeDailyTransactionsRetrievalRequestedService.accept(
           new OrangeDailyTransactionsRetrievalRequested(LocalDate.of(2026, 6, 29)));
+
     } catch (Exception e) {
       log.error("Failed to retrieve transactions, an error occured: ", e.getMessage());
       throw new RuntimeException("The error is " + e.getMessage());
